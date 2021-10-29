a39a78e3ae9cd8e13c5aed91d91eef1d51361554
Taha El Ghabi <tahaelghabi@outlook.com>
2021-10-29 09:33:11 +0100

    hello world

---
+++ JEE/demo/src/main/java/com/tp/java/demo/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.tp.java.demo;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
