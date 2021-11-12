58c1a65cba9aa4d623970b59182038de2eadaa04
Taha El Ghabi <tahaelghabi@outlook.com>
2021-11-12 12:08:21 +0100

    handler

--- JEE/demo/src/main/java/com/demo/HelloServlet.java
+++ JEE/demo/src/main/java/com/demo/HelloServlet.java
@@ -16,6 +16,7 @@ public class HelloServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
         String db = "test";
+
         // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
