0db1ba7e5422905a643793e0e0d0aa2c052361a5
KOBeerose <tahaelghabi@gmail.com>
2021-11-24 03:17:31 +0100

    first working demo

---
+++ CyberSecurity/front/src/HowToUse.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class HowToUse extends React.Component {
+    render() {
+        return (<div className='App-info-section'>
+        <h3>How to use?</h3>
+        <div>
+          Scan you Covid Vaccination Pass Either by pointing it to the camera or Uploading an image to the Platform
+          <br />
+        </div>
+      </div>);
+    }
+}
+
+export default HowToUse;
