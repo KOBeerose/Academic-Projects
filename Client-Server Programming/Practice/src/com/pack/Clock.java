d1c1ee4b1e99a2df46e83068d323cf9c4ea9fb3d
Taha El Ghabi <tahaelghabi@outlook.com>
2021-12-08 17:30:59 +0100

    Threads Java

---
+++ Client-Server Programming/Practice/src/com/pack/Clock.java
@@ -0,0 +1,17 @@
+package com.pack;
+
+import javax.swing.*;
+
+public class Verctor{
+    x`
+}
+public class Clock extends JLabel{
+    // to Complete
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Graphic Clock");
+        frame.setSize(200, 200);
+        frame.setContentPane(new Clock());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);}
+
+}
---
+++ Client-Server Programming/Practice/src/com/pack/PrintNumAlpha.java
@@ -0,0 +1,26 @@
+package com.pack;
+
+class PrintNums extends Thread {
+    public void run() {
+        for (int i = 1; i <= 260; i++) {
+            System.out.println(i + " ");
+        }
+    }
+}
+class PrintAlpha extends Thread {
+    public void run() {
+        char i;
+        for ( i = 'a'; i <= 'z'; i++) {
+            System.out.println(i + " ");
+        }
+    }
+}
+
+class PrintNumAlpha {
+    public static void main(String[] args) {
+        PrintNums t1 = new PrintNums();
+        PrintAlpha t2 = new PrintAlpha();
+        t1.start();
+        t2.start();
+    }
+}
