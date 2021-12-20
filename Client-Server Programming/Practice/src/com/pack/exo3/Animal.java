2b79ec30f82f959b7c83bf3b10d40f37b9cd582f
Taha El Ghabi <tahaelghabi@outlook.com>
2021-12-20 11:39:41 +0100

    numpy & pandas

---
+++ Client-Server Programming/Practice/src/com/pack/exo3/Animal.java
@@ -0,0 +1,33 @@
+package com.pack.exo3;
+
+import javax.swing.*;
+
+public class Animal extends Thread {
+
+    private int step;
+    public Animal (int step) {
+        super();
+        this.step = step;
+
+    }
+
+    public void run () {
+        running();
+    }
+    private void running() {
+        String threadName = Thread.currentThread().getName();
+
+        int dist;
+        for ( dist = 1; dist <= totalDistance; dist+=step) {
+            System.out.println(threadName+" : "+dist+" m");
+
+                if (threadName.equals("lievre") && dist == 25 ) {
+                    try {
+
+                    }
+                }
+        }
+
+    }
+}
+
---
+++ Client-Server Programming/Practice/src/com/pack/exo3/CourseDemo.java
@@ -0,0 +1,9 @@
+package com.pack.exo3;
+
+public class CourseDemo {
+
+    public static void main(String [] args) {
+
+        Thread
+    }
+}
