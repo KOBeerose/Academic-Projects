1813975f9ba47e5ec56090162f369119c0a88eca
KOBeerose <tahaelghabi@gmail.com>
2023-09-29 13:27:44 +0100

    Contains Duplicate

---
+++ Algorithms & CP/NeetCode/Arrays & Hashing/Contains Duplicate.py
@@ -0,0 +1,14 @@
+class Solution:
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        track = {}
+        for num in nums:
+            if num in track:
+                return True
+            else:
+                track[num]=True
+        return False
+
+
+nums = [1,1,1,3,3,4,3,2,4,2]
+mysolution = Solution()
+print(mysolution.containsDuplicate(nums))
