b37bd0071f3efa51f5154da1685aa849038b4a02
KOBeerose <tahaelghabi@gmail.com>
2023-10-04 23:23:29 +0100

    optimal group anagram

---
+++ Algorithms & CP/NeetCode/Arrays & Hashing/Group Anagram Optimal.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        hashmap = defaultdict(list)
+        for string in strs:
+            map = [0]*26
+            for char in string:
+                map[ord(char)-ord('a')]+=1
+            hashmap[tuple(map)].append(string)
+        return hashmap.values()
+
+
+                 
+       
+
+#strs = ["","b", 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k']
+strs = ["eat","tea","tan","ate","nat","bat"]
+solution = Solution()
+print(solution.groupAnagrams(strs))
--- Algorithms & CP/NeetCode/Arrays & Hashing/Group Anagram2.py
+++ Algorithms & CP/NeetCode/Arrays & Hashing/Group Anagram2.py
@@ -2,17 +2,15 @@ class Solution:
     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
         sorted_strs = [''.join(sorted(s)) for s in strs]
         track = {}
-        result = []
         for i in range(len(strs)):
             if sorted_strs[i] in track:
                 track[sorted_strs[i]].append(strs[i])
             else:
                 track[sorted_strs[i]] = [strs[i]]
-        for key in track: result.append(track[key])
-        return result
+        return track.values()
        
 
-strs = ["","b", 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k']
-#strs = ["eat","tea","tan","ate","nat","bat"]
+#strs = ["","b", 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k']
+strs = ["eat","tea","tan","ate","nat","bat"]
 solution = Solution()
 print(solution.groupAnagrams(strs))
