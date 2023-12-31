dfb59d09c27b4e8dd0c88e7e8e54a715fdeb25e6
KOBeerose <tahaelghabi@gmail.com>
2023-12-31 22:53:01 +0100

    Valid Sudoku

---
+++ Algorithms & CP/NeetCode/Arrays & Hashing/Product of Array Except Self Optimal.py
@@ -0,0 +1,14 @@
+class Solution:
+    def productExceptSelf(self, nums: list[int]) -> list[int]:
+        answer = [1] * len(nums)
+        multi = 1
+        for i in range(1, len(nums)):
+            answer[i] = answer[i-1]*nums[i-1]
+        for i in range(1, len(nums)):
+            multi *= nums[-i]
+            answer[-i-1] = answer[-i-1]*multi
+        return answer
+    
+nums = [1,2,3,4]
+solution = Solution()
+print(solution.productExceptSelf(nums))
